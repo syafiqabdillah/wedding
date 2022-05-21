70f55723d1fdbbb9833494a2d702be0f8677b1b2
syafiqjustika <syafiq@justika.com>
2022-05-21 19:33:29 +0800

    add mute toggle button

--- components/Audio.js
+++ components/Audio.js
@@ -1,31 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faVolumeMute, faVolumeUp } from '@fortawesome/free-solid-svg-icons'
+
+const VOLUME = 0.3
+
+function PauseButton({ show }) {
+  const [audio, setAudio] = useState(null)
+  const [mute, setMute] = useState(false)
+
+  useEffect(() => {
+    setAudio(document.getElementById('audio'))
+  }, [])
+
+  function onToggleMute() {
+    if (audio.volume != 0) {
+      audio.volume = 0
+      setMute(true)
+    } else {
+      audio.volume = VOLUME
+      setMute(false)
+    }
+  }
+
+  if (audio && show)
+    return (
+      <div
+        className="absolute cursor-pointer top-4 right-4 bg-blue-200 rounded-full h-10 w-10 flex justify-center items-center"
+        onClick={() => onToggleMute()}
+      >
+        <FontAwesomeIcon
+          icon={mute ? faVolumeMute : faVolumeUp}
+          className="h-3 text-blue-500"
+        />
+      </div>
+    )
+
+  return null
+}
 
 function Audio() {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(true)
+  const [showMuteToggle, setShowMuteToggle] = useState(false)
 
   function playSound() {
-    let audio = document.getElementById("audio");
-    audio.play();
-    setShow(false);
+    let audio = document.getElementById('audio')
+    audio.volume = VOLUME
+    audio.play()
+    setShow(false)
+    setShowMuteToggle(true)
   }
 
   return (
     <div
       className={`fixed h-screen w-screen bg-gray-600 top-0 left-0 flex justify-center transition-all duration-1000 items-center ${
-        show ? "bg-opacity-80" : "bg-opacity-0"
+        show ? 'bg-opacity-80' : 'bg-opacity-0'
       }`}
     >
       {show && (
         <div
           onClick={playSound}
-          className="bg-blue-200 font-macondo text-2xl cursor-pointer h-32 w-32 rounded-full flex justify-center items-center text-center z-10 opacity-100 animate-bounce"
+          className="bg-blue-200 hover:brightness-125 transition-all duration-300 font-macondo text-2xl cursor-pointer h-32 w-32 rounded-full flex justify-center items-center text-center z-10 opacity-100 "
         >
           Open Invitation
         </div>
       )}
+      <PauseButton show={showMuteToggle} />
       <audio src="/music/akuma-no-ko.mp3" id="audio" />
     </div>
-  );
+  )
 }
 
-export default Audio;
+export default Audio
--- components/Banner.js
+++ components/Banner.js
@@ -1,82 +1,12 @@
-import React from "react";
-import ReactAudioPlayer from "react-audio-player";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMusic } from "@fortawesome/free-solid-svg-icons";
-
-import { isSafari } from "../utils/browser";
-
-function Button(props) {
-  return (
-    <button
-      className={
-        props.className +
-        " bg-blue-200 px-8 rounded-lg bg-opacity-70 py-2 hover:bg-opacity-100 transition-all"
-      }
-    >
-      {props.children}
-    </button>
-  );
-}
-
-function SafariPlayer() {
-  const [muted, setMuted] = React.useState(true);
-
-  return (
-    <ReactAudioPlayer
-      src="/music/akuma-no-ko.mp3"
-      controls={true}
-      autoPlay={true}
-      muted={false}
-      loop={true}
-      volume={0.3}
-      className="absolute top-2"
-    />
-  );
-}
-
-function DefaultPlayer() {
-  const [showPlayer, setShowPlayer] = React.useState(false);
-  const [muted, setMuted] = React.useState(true);
-
-  React.useEffect(() => {
-    setTimeout(() => {
-      setMuted(false);
-    }, 1500);
-  }, []);
-
-  return (
-    <div className="absolute top-2 right-2 flex items-end">
-      <ReactAudioPlayer
-        src="/music/akuma-no-ko.mp3"
-        controls={showPlayer}
-        autoPlay={true}
-        muted={muted}
-        loop={true}
-        volume={0.3}
-        onPlay={() => console.log("play")}
-      />
-      <div
-        className="bg-blue-300 opacity-50 hover:opacity-80 text-white flex justify-center items-center rounded-full p-4 h-12 w-12 ml-2 cursor-pointer"
-        onClick={() => setShowPlayer(!showPlayer)}
-      >
-        <FontAwesomeIcon icon={faMusic} className="mr-[2px]" />
-      </div>
-    </div>
-  );
-}
+import React from 'react'
 
 function Banner() {
-  const [safari, setSafari] = React.useState(false)
-
-  React.useEffect(() => {
-    setSafari(isSafari())
-  }, [])
   return (
     <section className="min-h-screen flex flex-col justify-center items-center relative text-center">
       <img
         src="/img/afifasyafiq.webp"
         className="absolute -z-10 left-0 top-0 w-full h-full object-cover"
-        style={{ opacity: "50%" }}
+        style={{ opacity: '50%' }}
         alt=""
       />
       <p className="mb-4 font-extralight">The wedding of</p>
@@ -91,13 +21,8 @@ function Banner() {
           </div>
         </div>
       </h1>
-      {/* {safari ?
-        <SafariPlayer />
-        :
-        <DefaultPlayer />
-      } */}
     </section>
-  );
+  )
 }
 
-export default Banner;
+export default Banner
