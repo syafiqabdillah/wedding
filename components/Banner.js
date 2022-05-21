62647cd489011e0a6d6f7802af6a17d7f6b7da17
syafiqjustika <syafiq@justika.com>
2022-05-21 11:01:05 +0800

    add safari compability

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,9 @@
-import { faMusic } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import ReactAudioPlayer from "react-audio-player";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMusic } from "@fortawesome/free-solid-svg-icons";
+
+import { isSafari } from "../utils/browser";
 
 function Button(props) {
   return (
@@ -16,8 +18,59 @@ function Button(props) {
   );
 }
 
-function Banner() {
+function SafariPlayer() {
+  const [muted, setMuted] = React.useState(true);
+
+  return (
+    <ReactAudioPlayer
+      src="/music/akuma-no-ko.mp3"
+      controls={true}
+      autoPlay={true}
+      muted={false}
+      loop={true}
+      volume={0.3}
+      className="absolute top-2"
+    />
+  );
+}
+
+function DefaultPlayer() {
   const [showPlayer, setShowPlayer] = React.useState(false);
+  const [muted, setMuted] = React.useState(true);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setMuted(false);
+    }, 1500);
+  }, []);
+
+  return (
+    <div className="absolute top-2 right-2 flex items-end">
+      <ReactAudioPlayer
+        src="/music/akuma-no-ko.mp3"
+        controls={showPlayer}
+        autoPlay={true}
+        muted={muted}
+        loop={true}
+        volume={0.3}
+        onPlay={() => console.log("play")}
+      />
+      <div
+        className="bg-blue-300 opacity-50 hover:opacity-80 text-white flex justify-center items-center rounded-full p-4 h-12 w-12 ml-2 cursor-pointer"
+        onClick={() => setShowPlayer(!showPlayer)}
+      >
+        <FontAwesomeIcon icon={faMusic} className="mr-[2px]" />
+      </div>
+    </div>
+  );
+}
+
+function Banner() {
+  const [safari, setSafari] = React.useState(false)
+
+  React.useEffect(() => {
+    setSafari(isSafari())
+  }, [])
   return (
     <section className="min-h-screen flex flex-col justify-center items-center relative text-center">
       <img
@@ -29,26 +82,20 @@ function Banner() {
       <p className="mb-4 font-extralight">The wedding of</p>
       <h1 className="italic text-5xl">
         <div className="w-[130px] h-[180px] flex flex-col gap-3 justify-center">
-          <div data-aos="fade-right" className="font-macondo tracking-wider">Afifa</div>
+          <div data-aos="fade-right" className="font-macondo tracking-wider">
+            Afifa
+          </div>
           <div className="font-smooch">&</div>
-          <div data-aos="fade-left" className="font-macondo tracking-wider">Syafiq</div>
+          <div data-aos="fade-left" className="font-macondo tracking-wider">
+            Syafiq
+          </div>
         </div>
       </h1>
-      <div className="absolute top-2 right-2 flex items-end">
-        <ReactAudioPlayer
-          src="/music/akuma-no-ko.mp3"
-          controls={showPlayer}
-          autoPlay={true}
-          loop={true}
-          volume={.3}
-        />
-        <div
-          className="bg-blue-300 opacity-50 hover:opacity-80 text-white flex justify-center items-center rounded-full p-4 h-12 w-12 ml-2 cursor-pointer"
-          onClick={() => setShowPlayer(!showPlayer)}
-        >
-          <FontAwesomeIcon icon={faMusic} className="mr-[2px]" />
-        </div>
-      </div>
+      {safari ?
+        <SafariPlayer />
+        :
+        <DefaultPlayer />
+      }
     </section>
   );
 }
