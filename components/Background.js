ca60c55a59d39c6423f07e232bb45bccfed707ed
syafiqabdillah <abdillah.syafiq@gmail.com>
2022-08-07 17:58:00 +0700

    add flower and change default name and location

--- components/Background.js
+++ components/Background.js
@@ -4,7 +4,18 @@ import PauseButton from './PauseButton'
 
 function Background() {
   return (
-    <div className="fixed top-0 left-0 w-full h-screen -z-10 bg-paper"></div>
+    <div className="fixed top-0 left-0 w-full h-screen -z-10 bg-paper">
+      <img
+        className="-rotate-90 absolute top-2 w-[160px] -left-4"
+        src="/img/flower.png"
+        alt="flower 1"
+      />
+      <img
+        className="rotate-90 absolute bottom-24 -mb-2 w-[160px] -right-4"
+        src="/img/flower.png"
+        alt="flower 1"
+      />
+    </div>
   )
 }
 
