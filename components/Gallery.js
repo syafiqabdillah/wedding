ee5233c88e44aa8c14f16edc8c579c8c0e1a1536
syafiqabdillah <abdillah.syafiq@gmail.com>
2022-08-07 15:49:30 +0700

    update image gallery

--- components/Gallery.js
+++ components/Gallery.js
@@ -4,13 +4,13 @@ import ImageGallery from 'react-image-gallery'
 import Section from './Section'
 
 const THUMBNAIL_CLASS =
-  'h-[100px] w-[100px] cover overflow-hidden brightness-110'
+  'h-[100px] w-[100px] cover overflow-hidden object-bottom'
 
 function getImageData(src) {
   return {
-    original: `/img/prewed/${src}`,
-    originalClass: 'brightness-[150%]',
-    thumbnail: `/img/prewed/${src}`,
+    original: `/img/edited/${src}`,
+    originalClass: '',
+    thumbnail: `/img/edited/${src}`,
     thumbnailClass: THUMBNAIL_CLASS,
     loading: 'lazy',
     thumbnailLoading: 'lazy',
@@ -18,30 +18,25 @@ function getImageData(src) {
 }
 
 const FILE_NAMES = [
-  'DSCF0299.JPG',
-  'DSCF0323.JPG',
-  'DSCF0324.JPG',
-  'DSCF0325.JPG',
-  'DSCF0364.JPG',
-  'DSCF0403.JPG',
-  'DSCF0412.JPG',
-  'DSCF0414.JPG',
-  'DSCF0416.JPG',
-  'DSCF0517.JPG',
-  'DSCF0535.JPG',
+  'DSCF0274.JPG',
+  'DSCF0337.JPG',
+  'DSCF0413.JPG',
+  'DSCF0415.JPG',
+  'DSCF0471.JPG',
+  'DSCF0502.JPG',
+  'DSCF0510.JPG',
+  'DSCF0538.JPG',
   'DSCF0539.JPG',
+  'DSCF0578.JPG',
   'DSCF0586.JPG',
-  'DSCF0635.JPG',
   'DSCF0658.JPG',
-  'DSCF0669.JPG',
-  'DSCF0672.JPG',
-  'DSCF0722.JPG',
-  'DSCF0748.JPG',
+  'DSCF0689.JPG',
+  'DSCF0663.JPG',
+  'DSCF0693.JPG',
+  'DSCF0772.JPG',
   'DSCF0842.JPG',
   'DSCF0865.JPG',
-  'DSCF0877.JPG',
   'DSCF0915.JPG',
-  'DSCF0926.JPG',
 ]
 
 const IMAGES = FILE_NAMES.map((file) => getImageData(file))
